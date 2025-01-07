22bfa2212f8f7079c6c265764fd210ec9acce184
naseeftk <naseeftkvlm6899@gmail.com>
2025-01-07 09:39:17 +0530

    Time: 12 ms (44.32%), Space: 45.5 MB (73.86%) - LeetHub

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool CanBeEqual(int[] target, int[] arr) {
+       Array.Sort(target);
+         Array.Sort(arr);
+   
+      return target.SequenceEqual(arr);
+    
+       
+    }
+}
+
+            
+         
+        
+  
