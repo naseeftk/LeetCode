31ec7f5bbcf8c2f5bd01e3723dd98fa641475ad0
naseeftk <naseeftkvlm6899@gmail.com>
2025-01-10 10:05:27 +0530

    Time: 1 ms (87.09%), Space: 58.7 MB (5.19%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        int nonZeroIndex = 0;
+
+       
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
+            }
+        }
+
+        for (int i = nonZeroIndex; i < nums.Length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
