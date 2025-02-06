e56a7173a70e3b0f7581bf5b063218a9e7c880f2
naseeftk <naseeftkvlm6899@gmail.com>
2025-02-06 09:53:08 +0530

    Time: 2981 ms (75.35%), Space: 0B (100%) - LeetHub

---
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -0,0 +1,4 @@
+/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below */
+SELECT PRODUCT_NAME , YEAR , PRICE FROM SALES 
+JOIN PRODUCT ON SALES.PRODUCT_ID  = PRODUCT.PRODUCT_ID; 
