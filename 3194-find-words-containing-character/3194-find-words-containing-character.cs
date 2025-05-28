8b8f63586706c199912acbb7bf8a3caa5c69044a
naseeftk <naseeftkvlm6899@gmail.com>
2025-05-28 10:15:57 +0530

    Time: 0 ms (100%), Space: 53.7 MB (27.64%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public IList<int> FindWordsContaining(string[] words, char x) {
+        List<int> result=new List<int>();
+        for(int i=0;i<words.Length;i++){
+           
+                if(words[i].Contains(x)){
+result.Add(i);
+            }
+
+        }
+        return result;
+    }
+}
